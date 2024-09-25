4d8a5fcea80873a992adcd2b0e3f162ac3d9d32f
WaspSpook <a.gen.maximov@gmail.com>
2024-09-25 12:14:22 +0300

    добавил проект в dev1

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# задаем переменную с просьбой в вести год.
+year = int(input('Введите год: '))
+
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+    print('YES')
+else:
+    print('NO')
+
