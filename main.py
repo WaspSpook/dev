ba25bda706db473715000d811cb31f40c0d478e1
WaspSpook <a.gen.maximov@gmail.com>
2024-10-14 08:54:43 +0300

    словари

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-# работа со словами
+# Исходные данные
+items = {
+ 'milk15': {'name': 'молоко 1.5%', 'count': 34, 'price': 89.9},
+ 'cheese': {'name': 'сыр молочный 1 кг.', 'count': 12, 'price': 990.9},
+ 'sausage': {'name': 'колбаса 1 кг.', 'count': 122, 'price': 1990.9}
+}
 
-word = 'test'
-if len(word) % 2 == 1:
-    print(word[len(word) // 2])
-else:
-    print(word[len(word) // 2 - 1: len(word) // 2 + 1])
+# Формирование словаря с использованием dict comprehension
+result = {key: count < 20 for key, item in items.items() for count in item['count']}
 
+# Вывод результата
+print(result)
